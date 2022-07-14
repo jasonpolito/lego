3d5bb39e2a564d06b52ebae03ce534d10c447bad
Jason Polito <jason@placementlabs.com>
2022-07-14 19:30:13 -0400

    sync up

--- resources/views/site/blocks/form.blade.php
+++ resources/views/site/blocks/form.blade.php
@@ -17,8 +17,12 @@ $id = $block->input('block_id') ?? uniqid();
                     <div class="absolute top-0 right-0 p-2">
                         <a href="/admin/forms/{{ $form->id }}/edit"
                             style="z-index: 999; border: solid 1px rgba(255,255,255,.5)" target="_blank"
-                            class="flex flex-col items-center justify-center px-2 py-1 text-xs text-white transition bg-black rounded opacity-50 hover:opacity-100">
-                            Edit Form
+                            class="flex items-center justify-center px-2 py-1 text-xs text-white transition bg-black rounded opacity-50 hover:opacity-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-1" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                            </svg> Form
                         </a>
                     </div>
                     @endif
--- resources/views/site/blocks/header.blade.php
+++ resources/views/site/blocks/header.blade.php
@@ -51,8 +51,8 @@ $themes = [
                             class="absolute pb-4 text-sm transition bg-white rounded shadow-2xl opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 top-full whitespace-nowrap">
                             @foreach ($link->children()->orderBy('position')->get() as $link)
                             <li>
-                                <a style="min-width: 10rem" href="{!! $link->input('url') !!}"
-                                    class="block px-6 py-2 pr-8 transition hover:opacity-80">{!! $link->input('text')
+                                <a style="min-width: 10rem" href="{!! link_url($link) !!}"
+                                    class="block px-6 py-2 pr-8 transition hover:opacity-80">{!! link_text($link)
                                     !!}</a>
                             </li>
                             @endforeach
@@ -66,20 +66,20 @@ $themes = [
                         <a href="#" class="block p-6" @click.prevent="menuOpen = !menuOpen">
                             <div class="w-8 h-8">
                                 <svg xmlns="http://www.w3.org/2000/svg"
-                                    :class="{'opacity-0 rotate-90': menuOpen, 'opacity-100 rotate-0': !menuOpen}"
+                                    :class="{'opacity-0 -rotate-90': menuOpen, 'opacity-100 -rotate-0': !menuOpen}"
                                     class="w-8 h-8 transition duration-300 transform fill-parent" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
                                 </svg>
                                 <svg xmlns="http://www.w3.org/2000/svg"
-                                    :class="{'opacity-100 -rotate-0': menuOpen, 'opacity-0 -rotate-90': !menuOpen}"
+                                    :class="{'opacity-100 rotate-0': menuOpen, 'opacity-0 rotate-90': !menuOpen}"
                                     class="w-8 h-8 transition duration-300 transform opacity-0 fill-parent" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </div>
                         </a>
-                        <ul class="absolute right-0 pb-6 mr-2 transition transform -translate-y-4 bg-white rounded-lg shadow-xl opacity-0 pointer-events-none top-full"
+                        <ul class="absolute right-0 pb-6 mr-2 transition duration-300 transform -translate-y-8 bg-white rounded-lg shadow-xl opacity-0 pointer-events-none translat top-full"
                             :class="{'pointer-events-auto opacity-100 translate-y-0': menuOpen, 'pointer-events-none opacity-0 -translate-y-4': !menuOpen}">
                             @foreach ($block->children()->orderBy('position')->get() as $link)
                             <li>
@@ -116,8 +116,12 @@ $themes = [
     <div class="absolute top-0 right-0 p-2">
         <a href="/admin/partials/{{ $partial->id }}/edit" style="z-index: 999; border: solid 1px rgba(255,255,255,.5)"
             target="_blank"
-            class="flex flex-col items-center justify-center px-2 py-1 text-xs text-white transition bg-black rounded opacity-50 hover:opacity-100">
-            Edit Partial
+            class="flex items-center justify-center px-2 py-1 text-xs text-white transition bg-black rounded opacity-50 hover:opacity-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-1" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            </svg> Partial
         </a>
     </div>
     @endif
